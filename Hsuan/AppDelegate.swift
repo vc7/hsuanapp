c525f3545ba3a5fddfae45938bac09f338754b02
Vincent Chen <vince78718@gmail.com>
2015-04-14 23:37:57 +0800

    Initial Commit

---
+++ Hsuan/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Hsuan
+//
+//  Created by vincent on 2015/04/13.
+//  Copyright (c) 2015年 Vincenttt. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var mainTabBarController: UITabBarController?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.backgroundColor = UIColor.whiteColor()
+        
+        self.mainTabBarController = UITabBarController()
+        
+        self.window!.rootViewController = self.mainTabBarController
+        
+        self.window!.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
